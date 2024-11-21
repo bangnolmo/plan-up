5c09a7a4fbaf96c2629007c632c0a9fad8de7714
in-up <inup@kyonggi.ac.kr>
2024-11-22 00:37:36 +0900

    fix: edit column in the lectures database

--- src/app/(routes)/lecture/page.tsx
+++ src/app/(routes)/lecture/page.tsx
@@ -11,21 +11,19 @@ import FloatingButton from "@/app/_components/buttons/floatingButton";
 import { AddIcon } from "@/app/_components/Icon/AddIcon";
 
 const columns = [
-    { key: "lecture_name", label: "강의명" },
-    { key: "hakgwa_cd", label: "학과 코드" },
+    { key: "sub_num", label: "과목번호" },
+    { key: "name", label: "과목명" },
     { key: "grade", label: "학년" },
-    { key: "term", label: "학기" },
-    { key: "course_type", label: "이수 구분" },
-    { key: "credit", label: "학점" },
-    { key: "professor_name", label: "교수명" },
-    { key: "enrollment_status", label: "수강 상태" },
-    { key: "time", label: "강의 시간" },
-    { key: "location", label: "강의실 위치" },
+    { key: "course_type", label: "이수구분" },
+    { key: "credits", label: "학점" },
+    { key: "professor", label: "교수명" },
+    { key: "note", label: "비고" },
+    { key: "period", label: "교시" },
+    { key: "location", label: "강의실" },
 ];
 
 const DynamicLectureTable = () => {
     const [lectures, setLectures] = useState(mockLectures);
- 
 
     useEffect(() => {
         console.log(setLectures);
@@ -35,21 +33,13 @@ const DynamicLectureTable = () => {
         //   .then((data) => setLectures(data));
     }, []);
 
-
     return (
         <>
             <Header />
             <PageInfo title="개설과목 조회" description="개설 과목을 조회할 수 있어요." />
             <SearchForm />
             <ListView columns={columns} items={lectures} actionType="add" onActionButtonClick={handleCartAddClick} />
-            <FloatingButton
-                color="success"
-                ariaLabel=""
-                hovermsg=""
-                variant="faded"
-                icon={<AddIcon size={30}/>}
-                onPress={handleFloatingCartClick}
-            />
+            <FloatingButton color="success" ariaLabel="" hovermsg="" variant="faded" icon={<AddIcon size={30} />} onPress={handleFloatingCartClick} />
         </>
     );
 };
--- src/app/_components/listview/ListView.tsx
+++ src/app/_components/listview/ListView.tsx
@@ -2,14 +2,7 @@
 "use client";
 
 // ListView.tsx
-import {
-  Table,
-  TableHeader,
-  TableBody,
-  TableColumn,
-  TableRow,
-  TableCell,
-} from "@nextui-org/react";
+import { Table, TableHeader, TableBody, TableColumn, TableRow, TableCell } from "@nextui-org/react";
 import { DeleteIcon } from "../Icon/DeleteIcon";
 import { AddIcon } from "../Icon/AddIcon";
 import IconButton from "../buttons/IconButton";
@@ -17,76 +10,67 @@ import IconButton from "../buttons/IconButton";
 // import { getCookie, setCookie, updateCookie } from "@/app/_cookieManager/cookieManager";
 
 interface ListViewProps {
-  columns: { key: string; label: string }[];
-  items: any[];
-  actionType?: "none" | "delete" | "add";
-  onActionButtonClick?: (item: Record<string, string | number>) => void; // 추가된 prop
+    columns: { key: string; label: string }[];
+    items: any[];
+    actionType?: "none" | "delete" | "add";
+    onActionButtonClick?: (item: Record<string, string | number>) => void; // 추가된 prop
 }
 
 const ListView: React.FC<ListViewProps> = ({ columns, items, actionType = "none", onActionButtonClick }) => {
-  
-  // "액션" 열을 조건부로 추가
-  const dynamicColumns = actionType !== "none"
-    ? [...columns, { key: "action", label: "Action" }]
-    : columns;
+    // "액션" 열을 조건부로 추가
+    const dynamicColumns = actionType !== "none" ? [...columns, { key: "action", label: "Action" }] : columns;
 
-  return (
-    <div className="overflow-x-auto">
-      <Table aria-label="Dynamic Table" selectionMode="none">
-        <TableHeader columns={dynamicColumns}>
-          {(column) => (
-            <TableColumn key={column.key}>{column.label}</TableColumn>
-          )}
-        </TableHeader>
-        <TableBody items={items}>
-          {(item) => (
-            <TableRow key={item.id}>
-              {dynamicColumns.map((column) => (
-                column.key === "action" ? (
-                  <TableCell key={column.key}>
-                    {actionType === "delete" && (
-                        <IconButton
-                        aria-label="Delete item"
-                        onPress={() => {
-                          if (onActionButtonClick){
-                            onActionButtonClick(item);
-                          }
-                        }}
-                        icon={<DeleteIcon />}
-                        color="danger"
-                        ariaLabel="Delete"
-                        hovermsg="장바구니에서 삭제"
-                        size='sm'
-                      />
+    return (
+        <div className="overflow-x-auto">
+            <Table aria-label="Dynamic Table" selectionMode="none">
+                <TableHeader columns={dynamicColumns}>{(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}</TableHeader>
+                <TableBody items={items}>
+                    {(item) => (
+                        <TableRow key={item.sub_num}>
+                            {dynamicColumns.map((column) =>
+                                column.key === "action" ? (
+                                    <TableCell key={column.key}>
+                                        {actionType === "delete" && (
+                                            <IconButton
+                                                aria-label="Delete item"
+                                                onPress={() => {
+                                                    if (onActionButtonClick) {
+                                                        onActionButtonClick(item);
+                                                    }
+                                                }}
+                                                icon={<DeleteIcon />}
+                                                color="danger"
+                                                ariaLabel="Delete"
+                                                hovermsg="장바구니에서 삭제"
+                                                size="sm"
+                                            />
+                                        )}
+                                        {actionType === "add" && (
+                                            <IconButton
+                                                aria-label="Add item"
+                                                onPress={() => {
+                                                    if (onActionButtonClick) {
+                                                        onActionButtonClick(item);
+                                                    }
+                                                }}
+                                                icon={<AddIcon />}
+                                                color="primary"
+                                                ariaLabel="Add"
+                                                hovermsg="장바구니 추가"
+                                                size="sm"
+                                            />
+                                        )}
+                                    </TableCell>
+                                ) : (
+                                    <TableCell key={column.key}>{item[column.key as keyof typeof item]}</TableCell>
+                                )
+                            )}
+                        </TableRow>
                     )}
-                    {actionType === "add" && (
-                      <IconButton
-                        aria-label="Add item"
-                        onPress={() => {
-                          if (onActionButtonClick){
-                            onActionButtonClick(item);
-                          }
-                        }}
-                        icon={<AddIcon />}
-                        color="primary"
-                        ariaLabel="Add"
-                        hovermsg="장바구니 추가"
-                        size="sm"
-                      />
-                    )}
-                  </TableCell>
-                ) : (
-                  <TableCell key={column.key}>
-                    {item[column.key as keyof typeof item]}
-                  </TableCell>
-                )
-              ))}
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  );
+                </TableBody>
+            </Table>
+        </div>
+    );
 };
 
 export default ListView;

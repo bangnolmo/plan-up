5abdd86bdf244044c4e44af4d8511f37d37bd4c8
in-up <inup@kyonggi.ac.kr>
2024-11-24 02:17:34 +0900

    chore: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,25 +5,32 @@ import { Providers } from "./providers";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "플랜업 PlanUp",
-  description: "플랜업 시간표 마법사",
-  icons: {
-    icon: "/favicon.ico",
-  },
+    title: "경기대학교 PLANUP",
+    description: "플랜업 시간표 마법사",
+    icons: {
+        icon: "/favicon.ico",
+    },
+    openGraph: {
+        title: "경기대학교 PLANUP",
+        description: "시간표를 쉽게 만들어보세요.",
+        url: "http://34.47.112.126/",
+        images: [
+            {
+                url: "http://34.47.112.126/og-image.png",
+                alt: "OG-IMAGE",
+            },
+        ],
+    },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="ko">
-      <body className="min-h-screen flex justify-center">
-        <div className="max-w-[1024px] w-full">
-          <Providers>{children}</Providers>
-        </div>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="ko">
+            <body className="min-h-screen flex justify-center">
+                <div className="max-w-[1024px] w-full">
+                    <Providers>{children}</Providers>
+                </div>
+            </body>
+        </html>
+    );
 }

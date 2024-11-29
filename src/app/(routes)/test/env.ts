854fed7aaaac8dcd3bc20546b3cf1e5892d10192
heung115 <heung115@naver.com>
2024-11-29 20:10:00 +0900

    feat: env.ts 파일 삭제 및 SelectGeneral 컴포넌트에서 환경 변수 로그 추가

--- src/app/(routes)/test/env.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "Not defined",
-    });
-}
